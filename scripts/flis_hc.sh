c5c3fca7c96e1ea088536161875dca3a68503261
anonresearcher1 <mmorafah@eng.ucsd.edu>
2022-09-19 22:16:11 -0700

    codes

---
+++ scripts/flis_hc.sh
@@ -0,0 +1,29 @@
+#/bin/bash
+# dir='../save_results/fedis_hc/noniid-labeldir/cifar10'
+# if [ ! -e $dir ]; then
+# mkdir -p $dir
+# fi
+
+python ../main_FLIS_HC.py --trial=1 \
+--rounds=20 \
+--num_users=100 \
+--frac=0.1 \
+--local_ep=10 \
+--local_bs=10 \
+--lr=0.01 \
+--momentum=0.5 \
+--model=simple-cnn \
+--dataset=cifar10 \
+--datadir='../../data/' \
+--savedir='../save_results/fedis_hc/' \
+--partition='noniid-labeldir' \
+--alg='flis_hc' \
+--beta=0.5 \
+--local_view \
+--noise=0 \
+--cluster_alpha=0.45 \
+--nclasses=10 \
+--nsamples_shared=2500 \
+--gpu=0 \
+--print_freq=50 \
+2>&1 | tee $dir'/logs.txt'
